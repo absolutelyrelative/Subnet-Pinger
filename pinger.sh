46aa9bd97afed372a71e7509bb206cc27b0c7c38
Paolo <59060818+absolutelyrelative@users.noreply.github.com>
2021-05-21 11:02:42 +0200

    Cleaned up code in pinger.sh

--- pinger.sh
+++ pinger.sh
@@ -5,6 +5,7 @@ usage() { echo "Usage: ./pinger.sh [-N netmask] [-I IP] "; exit; }
 max_ip=$( dotted2int 255.255.255.255 )
 
 #ARGUMENT CHECK
+#TODO: *Actually* implement usage of arguments
 while getopts N: opts; do
 	case ${opts} in
 		N) CUSTOM_SUBNET=${OPTARG}
@@ -32,11 +33,8 @@ while getopts N: opts; do
     ;;
 esac
 done
-#Bitwise operator: $(( $a & $b ))
-#ifconfig | grep inet -w | grep -v 127.0.0.1
-#        inet 192.168.176.91  netmask 255.255.255.0  broadcast 192.168.176.255
 
-#ifconfig | grep inet -w | grep -v 127.0.0.1 | grep "[0-9]* netmask 255.255.255.0, check echo $?"
+#Attempt to load Netmask and local IP automatically
 ifconfig | grep inet -w | grep -v 127.0.0.1 | tr ' ' '\t'  > temp.bin
 
 if [ $? == 0 ] #SUCCESS
@@ -44,32 +42,28 @@ then
 	raw_mask=$( cut -f13,13 temp.bin ) #Get Mask
 	if [ $? == 0 ] #MASK SUCCESS
 	then
-		echo "Rilevata automaticamente Netmask: " $raw_mask
+		echo "Netmask automatically detected: " $raw_mask
 		mask=$( dotted2int $raw_mask )
 		raw_ip=$( cut -f10,10 temp.bin )	#Get IP
 		if [ $? == 0 ] #IP SUCCESS
 		then
-			echo "Rilevato automaticamente IP: " $raw_ip
+			echo "Local IP automatically detected: " $raw_ip
 			ip=$( dotted2int $raw_ip )
 			ip_ctr=$(( $mask & $ip ))	#Bitwise AND
-			echo "Inizio scan da $ip_ctr a $max_ip. I risultati sono stampati su output.txt automaticamente"
+			echo "Starting scan from $ip_ctr to $max_ip. Results will be added to output.txt progressively."
 			for((ctr=$ip_ctr; ctr < $max_ip; ctr++))
 			do
-				#ping -4 -a -ttl 4 -c 1 $ctr
-				#temp=$( (ping -a -c 1 -i 200 -b $ctr) ) #Indirizzi di broadcast sono inclusi, non sappiamo quante subnet ci siano
-				#echo $temp >> temp2.txt
 				ping -a -W 1 -c 1 -i 200 -b $ctr | grep "from" >> output.txt &
 				#kill $!
-				#ping -a -c 1 -i 200 -b $ctr | grep "from" & >> output.txt
 			done
-			echo "Completato, controlla output.txt"
+			echo "Completed, check output.txt"
 
 		else
-			echo "Impossibile estrarre Netnmask. Puoi specificarla con l'argomento -N [Netmask]"
+			echo "Automatic extraction of Subnet Mask was not successful. Try manually specifying it with -N [Netmask]"
 		fi
 	else
-		echo "Impossibile estrarre IP. Puoi specificarlo con l'argomento -I [IP]"
+		echo "Automatic extraction of Local IP address was not successful. Try manually specifying it with -I [IP]"
 	fi
 	else
-		echo "Impossibile estrarre Netnmask & IP automaticamente."
+		echo "Automatic extraction of Subnet Mask and Local IP unsuccessful. Try manually specifying them with -I [IP] -N [Netmask]"
 fi
