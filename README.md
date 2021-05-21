dfbf4a38ff754f440ea66d1bf25c367c7180beb8
Paolo <59060818+absolutelyrelative@users.noreply.github.com>
2021-05-21 22:43:31 +0200

    Implemented Arguments

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Subnet Pinger
- My own implementation of an active-device finder
+ My own implementation of an active-device finder.
+ It launches multiple processes for each ping to speed up the process.
+ Watch out, you might DOS your own subnet! I'm not responsible for damages if so, consider yourselves warned! :D
+
+# Usage
+./pinger.sh [-n netmask -i IP]
+
+Example:
+./pinger.sh -n 255.255.255.0 -i 192.168.2.1
+
+
+# Files
+IPTools.sh -> contains the function to convert from quadruple-dotted-decimal to integer
+pinger.sh -> contains the actual code to ping
--- pinger.sh
+++ pinger.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 source IPTools.sh
-usage() { echo "Usage: ./pinger.sh [-N netmask] [-I IP] "; exit; }
+usage() { echo "Usage: ./pinger.sh [-n netmask -i IP] "; exit; }
 
 max_ip=$( dotted2int 255.255.255.255 )
+CUSTOM_SUBNET=0
+CUSTOM_IP=0
 
 #ARGUMENT CHECK
 #TODO: *Actually* implement usage of arguments
-while getopts N: opts; do
-	case ${opts} in
-		N) CUSTOM_SUBNET=${OPTARG}
-		if [ $WHICH_PRINT -gt $max_ip ]
-		then
-			usage
-		fi
-		if [ $WHICH_PRINT -lt 0 ]
+while getopts "n:i:" opts; do
+	case "${opts}" in
+		n) CUSTOM_SUBNET=${OPTARG}
+		TEMP1=$( dotted2int ${CUSTOM_SUBNET} )
+		CUSTOM_SUBNET=${TEMP1}
+		if [ ${CUSTOM_SUBNET} -gt ${max_ip} ] || [ ${CUSTOM_SUBNET} -lt 0 ]
 		then
 			usage
 		fi
 		;;
-		I) CUSTOM_IP=${OPTARG}
-		if [ $WHICH_PRINT -gt $max_ip ]
-		then
-			usage
-		fi
-		if [ $WHICH_PRINT -lt 0 ]
+		i) CUSTOM_IP=${OPTARG}
+		TEMP2=$( dotted2int ${CUSTOM_IP} )
+		CUSTOM_IP=$TEMP2
+		if [ $CUSTOM_IP -gt $max_ip ] || [ $CUSTOM_IP -lt 0 ]
 		then
 			usage
 		fi
@@ -33,37 +31,62 @@ while getopts N: opts; do
     ;;
 esac
 done
-
-#Attempt to load Netmask and local IP automatically
-ifconfig | grep inet -w | grep -v 127.0.0.1 | tr ' ' '\t'  > temp.bin
-
-if [ $? == 0 ] #SUCCESS
+if [ $CUSTOM_SUBNET -eq 0 ] || [ $CUSTOM_IP -eq 0 ] #No custom rule set
 then
-	raw_mask=$( cut -f13,13 temp.bin ) #Get Mask
-	if [ $? == 0 ] #MASK SUCCESS
-	then
-		echo "Netmask automatically detected: " $raw_mask
-		mask=$( dotted2int $raw_mask )
-		raw_ip=$( cut -f10,10 temp.bin )	#Get IP
-		if [ $? == 0 ] #IP SUCCESS
-		then
-			echo "Local IP automatically detected: " $raw_ip
-			ip=$( dotted2int $raw_ip )
-			ip_ctr=$(( $mask & $ip ))	#Bitwise AND
-			echo "Starting scan from $ip_ctr to $max_ip. Results will be added to output.txt progressively."
-			for((ctr=$ip_ctr; ctr < $max_ip; ctr++))
-			do
-				ping -a -W 1 -c 1 -i 200 -b $ctr | grep "from" >> output.txt &
-				#kill $!
-			done
-			echo "Completed, check output.txt"
+	#Attempt to load Netmask and local IP automatically
+	ifconfig | grep inet -w | grep -v 127.0.0.1 | tr ' ' '\t'  > temp.bin
 
+	if [ $? -eq 0 ] #SUCCESS
+	then
+		raw_mask=$( cut -f13,13 temp.bin ) #Get Mask
+		if [ $? -eq 0 ] #MASK SUCCESS
+		then
+			echo "Netmask automatically detected: " $raw_mask
+			mask=$( dotted2int $raw_mask )
+			raw_ip=$( cut -f10,10 temp.bin )	#Get IP
+			if [ $? -eq 0 ] #IP SUCCESS
+			then
+				echo "Local IP automatically detected: " $raw_ip
+				ip=$( dotted2int $raw_ip )
+				ip_ctr=$(( $mask & $ip ))	#Bitwise AND
+				echo "Starting scan from $ip_ctr to $max_ip. Results will be added to output.txt progressively."
+				for((ctr=$ip_ctr; ctr < $max_ip; ctr++))
+				do
+					ping -a -W 1 -c 1 -i 200 -b $ctr | grep "from" >> output.txt &
+					#kill $!
+				done
+				echo "Completed, check output.txt"
+
+			else
+				echo "Automatic extraction of Subnet Mask was not successful. Try manually specifying it with -N [Netmask]"
+			fi
 		else
-			echo "Automatic extraction of Subnet Mask was not successful. Try manually specifying it with -N [Netmask]"
+			echo "Automatic extraction of Local IP address was not successful. Try manually specifying it with -I [IP]"
 		fi
-	else
-		echo "Automatic extraction of Local IP address was not successful. Try manually specifying it with -I [IP]"
+		else
+			echo "Automatic extraction of Subnet Mask and Local IP unsuccessful. Try manually specifying them with -I [IP] -N [Netmask]"
 	fi
-	else
-		echo "Automatic extraction of Subnet Mask and Local IP unsuccessful. Try manually specifying them with -I [IP] -N [Netmask]"
+fi
+if [ $CUSTOM_SUBNET -ne 0 ] && [ $CUSTOM_IP -eq 0 ]
+then
+	echo "You have to specify a Custom IP too."
+	usage
+fi
+if [ $CUSTOM_SUBNET -eq 0 ] && [ $CUSTOM_IP -ne 0 ]
+then
+	echo "You have to specify a Custom Subnet too."
+	usage
+fi
+if [ $CUSTOM_SUBNET -ne 0 ] && [ $CUSTOM_IP -ne 0 ]
+then
+	mask=$( dotted2int $CUSTOM_SUBNET )
+	ip=$( dotted2int $CUSTOM_IP )
+	ip_ctr=$(( $mask & $ip ))	#Bitwise AND
+	echo "Starting scan from $ip_ctr to $max_ip. Results will be added to output.txt progressively."
+	for((ctr=$ip_ctr; ctr < $max_ip; ctr++))
+	do
+		ping -a -W 1 -c 1 -i 200 -b $ctr | grep "from" >> output.txt &
+		#kill $!
+	done
+	echo "Completed, check output.txt"
 fi
